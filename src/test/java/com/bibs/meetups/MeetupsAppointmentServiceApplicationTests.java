181c80c3ce0df157cb30e0287f7b7c2b49169530
beatriz <bcastelo@brivia.com.br>
2022-04-24 17:26:37 -0300

    first commit

---
+++ src/test/java/com/bibs/meetups/MeetupsAppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibs.meetups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupsAppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
