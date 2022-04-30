d52961596bb2b757a4d9cbc661ba97d23dae7615
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-04-30 18:49:53 -0300

    :bug: bug fix

--- src/test/java/com/bibs/meetups/service/RegistrationServiceTest.java
+++ src/test/java/com/bibs/meetups/service/RegistrationServiceTest.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 @ActiveProfiles("test")
 public class RegistrationServiceTest {
 
-    RegistrationRepository registrationService;
+    RegistrationService registrationService;
 
     @MockBean
     RegistrationRepository repository;
