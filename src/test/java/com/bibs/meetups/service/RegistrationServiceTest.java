104059556da6c436e90583f2fd66bb597280fc5a
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-05-02 21:30:55 -0300

    :test_tube: adding a failing test that needs refactor

--- src/test/java/com/bibs/meetups/service/RegistrationServiceTest.java
+++ src/test/java/com/bibs/meetups/service/RegistrationServiceTest.java
@@ -1,18 +1,29 @@
 package com.bibs.meetups.service;
 
+import com.bibs.meetups.exception.BusinessException;
 import com.bibs.meetups.model.entity.Registration;
 import com.bibs.meetups.repository.RegistrationRepository;
 import com.bibs.meetups.service.impl.RegistrationServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
@@ -58,6 +69,23 @@ public class RegistrationServiceTest {
                 .dateOfRegistration(LocalDate.now())
                 .registration("001")
                 .build();
+    }
+
+    @Test
+    @DisplayName("Should throw business error when try to save a new registration " +
+            "with a duplicated registration")
+    public void shouldNotSaveAsDuplicatedRegistration() {
+
+        Registration registration = createValidRegistration();
+        Mockito.when(repository.existsByRegistration(Mockito.any())).thenReturn(true);
+
+        Throwable exception = Assertions.catchThrowable( () -> registrationService.save(registration));
+        assertThat(exception)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("Registration already created!");
+
+        // + uma etapa de verificação (para nunca salvar):
+        Mockito.verify(repository, Mockito.never()).save(registration);
     }
 
 }
