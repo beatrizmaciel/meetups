e3132ccc0e801b456ff0a31123ff1b372dae49c1
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-05-09 22:28:37 -0300

    :sparkles: creating ApplicationControllerAdvice to deal w/ exceptions

---
+++ src/main/java/com/bibs/meetups/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.bibs.meetups.controller;
+
+import com.bibs.meetups.controller.exceptions.ApiErrors;
+import com.bibs.meetups.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidateException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult(); // pega argumentos não válidos
+
+        return new ApiErrors(bindingResult);
+    }
+
+    @ExceptionHandler(BusinessException.class) // mesmo parâmetro de dentro
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleBusinessException(BusinessException e) {
+        return new ApiErrors(e);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus
+    public ApiErrors handleResponseStatusException(ResponseStatusException e) {
+        return new ApiErrors(e);
+    }
+
+}
